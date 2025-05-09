c8ee0154ebe736d59fbef94e40d789363dc31cc6
Damian Sikorski <sikorski65@gmail.com>
2025-05-09 10:46:24 +0200

    base finie

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+// déclaration des variables
+
 const nomDepense = document.getElementById("nomDepense");
 const montantDepense = document.getElementById("montantDepense");
 const categorie = document.getElementById("categorie");
@@ -5,11 +7,70 @@ const addButton = document.getElementById("addButton");
 const liste = document.getElementById("liste");
 const total = document.getElementById("total");
 
-console.log(nomDepense);
-console.log(montantDepense);
-console.log(categorie);
-console.log(addButton);
-console.log(liste);
-console.log(total);
+const tabListe = [];
+
+// fonction qui return le contenu html
+
+function htmlContent(el1,el2,el3,ind){
+    return `<li date-index = ${ind}>${el1} - ${el2}€ - ${el3} <span class="delButton" id=${ind}>❌</span></li>`
+
+}
+
+// fonction permettant l'affichage du contenu du tableau
+function displayListe(){
+    if (tabListe.length == 0){
+        liste.innerHTML = "Aucun enregistrement";
+        return
+    }
+    // reinitialisation de l'affichage
+    liste.innerHTML = "";
+    // ajout de chaque Elementdu tableau à la liste
+    tabListe.forEach((e,index) =>{
+        liste.innerHTML += htmlContent(e[0],e[1],e[2],index);
+    })
+
+    total.textContent = TotalCalc();
+}
+
+//fonction qui calcule le total
+
+function TotalCalc(){
+    let sum = 0;
+    tabListe.forEach((element) =>{
+        sum += Number(element[1]);
+    })
+    return sum;
+}
+
+//bouton del
+
+liste.addEventListener("click",(event)=>{
+    event.preventDefault();
+    
+    if (event.target.classList.contains("delButton")){
+         let delButtonIndex = parseInt(event.target.id);
+         tabListe.splice(delButtonIndex,1)
+    }
+    else {return}
+    displayListe()
+    
+    
+    
+})
+
+// addButton
+
+addButton.addEventListener("click",(event) =>{
+    event.preventDefault();
+    tabListe.push([nomDepense.value,montantDepense.value,categorie.value])
+    displayListe()
+    console.log(TotalCalc());
+    
+})
+
+// initialisation du total
+liste.innerHTML = "Aucun enregistrement"; 
+total.textContent = 0;
+
 
 
