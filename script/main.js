d46ef5f71bbd742294c08c92adab285a6af8b5dc
Damian Sikorski <sikorski65@gmail.com>
2025-05-09 11:42:13 +0200

    deux bonus

--- script/main.js
+++ script/main.js
@@ -6,14 +6,34 @@ const categorie = document.getElementById("categorie");
 const addButton = document.getElementById("addButton");
 const liste = document.getElementById("liste");
 const total = document.getElementById("total");
+const date = document.getElementById("date");
 
 const tabListe = [];
 
 // fonction qui return le contenu html
 
-function htmlContent(el1,el2,el3,ind){
-    return `<li date-index = ${ind}>${el1} - ${el2}€ - ${el3} <span class="delButton" id=${ind}>❌</span></li>`
+function htmlContent(el1,el2,el3,el4,ind){
+    return `<li style="color:${colorPicker(el3)}">${el1} - ${el2}€ - ${el3} date:${el4}<span class="delButton" id=${ind}>❌</span></li>`
 
+}
+
+// fonction qui choisit la couleur en fonction de la catégorie
+function colorPicker(cat){
+    let color;
+    switch (cat){
+        case "transport":
+            color = "blue";
+            break
+        case "logement":
+            color = "green";
+            break
+        case "divertissement":
+            color = "pink";
+            break
+        default :
+            color = "black";
+    }
+    return color;
 }
 
 // fonction permettant l'affichage du contenu du tableau
@@ -27,7 +47,7 @@ function displayListe(){
     liste.innerHTML = "";
     // ajout de chaque Elementdu tableau à la liste
     tabListe.forEach((e,index) =>{
-        liste.innerHTML += htmlContent(e[0],e[1],e[2],index);
+        liste.innerHTML += htmlContent(e[0],e[1],e[2],e[3],index);
     })
 
     total.textContent = TotalCalc();
@@ -63,16 +83,16 @@ liste.addEventListener("click",(event)=>{
 
 addButton.addEventListener("click",(event) =>{
     event.preventDefault();
-    tabListe.push([nomDepense.value,montantDepense.value,categorie.value])
+    tabListe.push([nomDepense.value,montantDepense.value,categorie.value,date.value])
     displayListe()
     console.log(TotalCalc());
     nomDepense.value = "";
     montantDepense.value = "";
-    categorie.value ="";
+    
     
 })
 
-// initialisation du total
+// initialisation du total et de la liste
 liste.innerHTML = "Aucun enregistrement"; 
 total.textContent = 0;
 
