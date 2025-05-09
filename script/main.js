793bf58837be5af28b5ef34eadd5503110a59213
Damian Sikorski <sikorski65@gmail.com>
2025-05-09 09:11:06 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,15 @@
+const nomDepense = document.getElementById("nomDepense");
+const montantDepense = document.getElementById("montantDepense");
+const categorie = document.getElementById("categorie");
+const addButton = document.getElementById("addButton");
+const liste = document.getElementById("liste");
+const total = document.getElementById("total");
+
+console.log(nomDepense);
+console.log(montantDepense);
+console.log(categorie);
+console.log(addButton);
+console.log(liste);
+console.log(total);
+
+
